9dd2d44316217e302c6e1ea5cdde7788625007ea
Morten Heine Sørensen <mortenheinesorensen@Mortens-MacBook-Pro.local>
2022-08-07 17:05:50 +0200

    8 parallel threads

--- source/wordle.sh
+++ source/wordle.sh
@@ -1,9 +1,9 @@
 python run.py 0   11 &> ../output/00000_00011.txt &    
-#python run.py 0   12 &> ../output/00000_00012.txt &    
-#python run.py 13  25 &> ../output/00013_00025.txt &    
-#python run.py 26  38 &> ../output/00026_00038.txt &    
-#python run.py 39  49 &> ../output/00039_00049.txt &    
-#python run.py 50  62 &> ../output/00050_00062.txt &    
-#python run.py 63  75 &> ../output/00063_00075.txt &    
-#python run.py 76  88 &> ../output/00076_00088.txt &    
-#python run.py 89 100 &> ../output/00089_00100.txt &    
+python run.py 0   12 &> ../output/00000_00012.txt &    
+python run.py 13  25 &> ../output/00013_00025.txt &    
+python run.py 26  38 &> ../output/00026_00038.txt &    
+python run.py 39  49 &> ../output/00039_00049.txt &    
+python run.py 50  62 &> ../output/00050_00062.txt &    
+python run.py 63  75 &> ../output/00063_00075.txt &    
+python run.py 76  88 &> ../output/00076_00088.txt &    
+python run.py 89 100 &> ../output/00089_00100.txt &    
