cfeef66060e6eef127d6909d34db5b7349aa547c
Morten Heine Sørensen <mortenheinesorensen@Mortens-MacBook-Pro.local>
2022-08-12 21:25:00 +0200

    corrections

--- source/run.py
+++ source/run.py
@@ -1,8 +1,9 @@
 import sys
-from wordle import play_all_startwords
-from files import get_score_file, get_word_file
+from decision_trees import empty_decision_tree
+from wordle import play_all_startwords, play_game, best_guess_and_elimination, worst_elimination
+from files import get_score_file, get_word_file, all_words, all_scores
 
-def run_startwords():
+def run_play_all_startwords():
     min_startword = 6096 # LARES
     max_startword = 6096 # LARES
     if len(sys.argv)==3:
@@ -10,4 +11,37 @@ def run_startwords():
         max_startword  = int(sys.argv[2])
     play_all_startwords(26,5,get_word_file(),get_score_file(),min_startword,max_startword,True)
 
-run_startwords()
+
+def run_play_game():
+    start_word = 'LARES' # LARES
+    secret = 'WANGS'
+    combinations = []
+    inv_combinations = {}
+    all_words(26,5,combinations, inv_combinations, get_word_file())
+    scores = []
+    all_scores(26, 5, scores, get_score_file())
+    decision_tree = empty_decision_tree(start_word) 
+    play_game(combinations,inv_combinations,26,5,scores,decision_tree,start_word,secret)
+        
+    
+def run_best_guess_and_elimination():
+    combinations = []
+    inv_combinations = {}
+    all_words(26,5,combinations, inv_combinations, get_word_file())
+    scores = []
+    all_scores(26, 5, scores, get_score_file())
+    best_guess_and_elimination(combinations,inv_combinations, combinations,26,5,scores)
+
+def run_worst_elimination():
+    combinations = []
+    inv_combinations = {}
+    all_words(26,5,combinations, inv_combinations, get_word_file())
+    scores = []
+    all_scores(26, 5, scores, get_score_file())
+    worst_elimination('LARES',combinations,26,5,1000000,inv_combinations,scores)
+    
+
+#run_play_all_startwords()
+#run_play_game()
+#run_best_guess_and_elimination()
+run_worst_elimination()
--- source/wordle.sh
+++ source/wordle.sh
@@ -1,9 +1,17 @@
-python run.py 0   11 &> ../output/00000_00011.txt &    
-python run.py 0   12 &> ../output/00000_00012.txt &    
-python run.py 13  25 &> ../output/00013_00025.txt &    
-python run.py 26  38 &> ../output/00026_00038.txt &    
-python run.py 39  49 &> ../output/00039_00049.txt &    
-python run.py 50  62 &> ../output/00050_00062.txt &    
-python run.py 63  75 &> ../output/00063_00075.txt &    
-python run.py 76  88 &> ../output/00076_00088.txt &    
-python run.py 89 100 &> ../output/00089_00100.txt &    
+python run.py 3900 3999 &> ../output/03900_03999.txt &    
+python run.py 4000 4099 &> ../output/04000_04099.txt &    
+python run.py 4100 4199 &> ../output/04100_04199.txt &    
+python run.py 4200 4299 &> ../output/04200_04299.txt &    
+python run.py 4300 4399 &> ../output/04300_04399.txt &    
+python run.py 4400 4499 &> ../output/04400_04499.txt &    
+python run.py 4500 4599 &> ../output/04500_04599.txt &    
+python run.py 4600 4699 &> ../output/04600_04699.txt &    
+python run.py 4700 4799 &> ../output/04700_04799.txt &    
+python run.py 4800 4899 &> ../output/04800_04899.txt &    
+python run.py 4900 4999 &> ../output/04900_04999.txt &    
+python run.py 5000 5099 &> ../output/05000_05099.txt &    
+python run.py 5100 5199 &> ../output/05100_05199.txt &    
+python run.py 5200 5299 &> ../output/05200_05299.txt &    
+python run.py 5300 5399 &> ../output/05300_05399.txt &    
+python run.py 5400 5499 &> ../output/05400_05499.txt &    
+ 
